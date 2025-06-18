fa91fd653977c23c8d53d20800ee97da02209521
Brandon Algarra <balgarra@csumb.edu>
2025-06-17 17:51:07 -0700

    installed rnaCanvas, attempted to use getMotif

--- RNAMCTest/src/app/components/BabylonExamples/BasicScene.ts
+++ RNAMCTest/src/app/components/BabylonExamples/BasicScene.ts
@@ -1,5 +1,8 @@
 import { FreeCamera, HemisphericLight, MeshBuilder, Vector3, Scene, Engine, Mesh, VertexData } from "@babylonjs/core";
 // import motifData from 'public\json\1Y26.json'
+import { getMotif, Motif, MotifProps, Quat, Vec3 } from '@judah-silva/rnacanvas'
+
+
 export class BasicScene {
 
     scene: Scene;
@@ -7,15 +10,19 @@ export class BasicScene {
 
     constructor(private canvas: HTMLCanvasElement) {
         this.engine = new Engine(this.canvas, true);
-        this.scene = this.CreateScene();
-
-        this.engine.runRenderLoop(() =>{
-            this.scene.render();
-        })
+        // this.scene = this.CreateScene();
+        this.init();
     }
 
+    private async init() {
+  this.scene = await this.CreateScene();
+  this.engine.runRenderLoop(() => {
+    this.scene.render();
+  });
+}
 
-    CreateScene(): Scene {
+// Check if async is necessary
+    async CreateScene(): Promise<Scene> {
         const scene = new Scene(this.engine);
         const camera = new FreeCamera("camera", new Vector3(0,1,-5), scene);
         camera.attachControl(this.canvas, true);
@@ -27,6 +34,22 @@ export class BasicScene {
 
         const ball = MeshBuilder.CreateSphere("ball", {diameter: 1}, scene);
         ball.position = new Vector3(0,1,0);
+
+    //     const motif1: Motif = await getMotif(
+    //         '1Y26.json',
+    //          '#FF0000',
+    //    );
+    //    console.log(motif1);
+
+    //    const props: MotifProps = {
+    //          motif: motif1,
+    //             locked: false,
+    //    };
+
+      
+    //    scene.addMesh(motif1);
+    //    motif1.mesh.position = new Vector3(0, 1, 0);
+
 
         // const motifData = JSON.parse('1Y26.json');
         // const response = await fetch('/json/1Y26.json');
